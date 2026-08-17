c975e69f140af9eb582ce373c8d5a03e186ddec8
Mik Delies <mik@voltvroom.nl>
2026-08-17 12:58:37 +0200

    Add mobile conversion action bar

---
+++ components/MobileActionBar.tsx
@@ -0,0 +1,6 @@
+export function MobileActionBar() {
+  return <div className="mobileActionBar" aria-label="Snelle acties">
+    <a href="tel:+31502113883" data-vv-event="phone_click">Bel ons</a>
+    <a href="/keuzehulp" data-vv-event="match_started">Vind mijn auto</a>
+  </div>;
+}
