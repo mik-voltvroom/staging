c680cf0e91fe191c89d6c145c3a8fedcaeaf1942
Mik Delies <mik@voltvroom.nl>
2026-08-17 13:36:08 +0200

    Add VVOS Social Video dashboard page

---
+++ app/dashboard/social-video/page.tsx
@@ -0,0 +1,7 @@
+import { SocialVideoManager } from "@/components/dashboard/SocialVideoManager";
+
+export const metadata = { title: "Social & Video | VVOS" };
+
+export default function SocialVideoDashboardPage() {
+  return <SocialVideoManager />;
+}
