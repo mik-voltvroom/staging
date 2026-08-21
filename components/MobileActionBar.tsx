5cd2f79717c55a4189b436a29a215399f6176995
Mik Delies <mik@voltvroom.nl>
2026-08-21 23:19:46 +0200

    feat(frontend): make mobile contact actions persistent

--- components/MobileActionBar.tsx
+++ components/MobileActionBar.tsx
@@ -1,6 +1,8 @@
 export function MobileActionBar() {
+  const routeHref = "https://www.google.com/maps/dir/?api=1&destination=Euvelgunnerweg%2050%2C%209723%20CW%20Groningen&travelmode=driving";
   return <div className="mobileActionBar" aria-label="Snelle acties">
-    <a href="tel:+31502113883" data-vv-event="phone_click">Bel ons</a>
-    <a href="/keuzehulp" data-vv-event="match_started">Vind mijn auto</a>
+    <a href="tel:+31502113883" data-vv-event="phone_click">Bel</a>
+    <a href="/#advies" data-vv-event="chat_click">Chat</a>
+    <a href={routeHref} target="_blank" rel="noopener noreferrer" data-vv-event="route_click">Route</a>
   </div>;
 }
